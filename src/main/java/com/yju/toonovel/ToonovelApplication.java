c6664d5cfe6e419f36e42ca7edc54fabda91dddd
Hannah0su <luvmyselfh@gmail.com>
2023-03-30 11:40:37 +0900

    :tada: 프로젝트 생성

---
+++ src/main/java/com/yju/toonovel/ToonovelApplication.java
@@ -0,0 +1,13 @@
+package com.yju.toonovel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToonovelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToonovelApplication.class, args);
+	}
+
+}
