0ce6514d80a61caea7d7cc16b9fca031958ccb3a
YJU-KimJeongSu <103088862+YJU-KimJeongSu@users.noreply.github.com>
2023-04-12 17:24:48 +0900

    :bug: HTTP 메서드 연결 잘못된 문제 수정

--- src/main/java/com/yju/toonovel/domain/post/controller/PostController.java
+++ src/main/java/com/yju/toonovel/domain/post/controller/PostController.java
@@ -7,7 +7,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -40,7 +39,7 @@ public class PostController {
 	}
 
 	@PatchMapping("/update")
-	public void uppdate(@RequestBody PostRequestDto dto) {
+	public void update(@RequestBody PostRequestDto dto) {
 		communityService.updatePost(dto);
 	}
 
