ac13e7c5555aaac2e525530c13177cfccc0f941f
Hannah0su <luvmyselfh@gmail.com>
2023-04-17 11:37:12 +0900

    :wrench: SecurityConfig 수정

--- src/main/java/com/yju/toonovel/global/config/SecurityConfig.java
+++ src/main/java/com/yju/toonovel/global/config/SecurityConfig.java
@@ -5,21 +5,46 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.oauth2.client.web.OAuth2AuthorizationRequestRedirectFilter;
 import org.springframework.security.web.SecurityFilterChain;
 
+import com.yju.toonovel.global.security.jwt.filter.JwtAuthenticationEntryPoint;
+import com.yju.toonovel.global.security.jwt.filter.JwtAuthenticationFilter;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
+	private final JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
+	private final JwtAuthenticationFilter jwtAuthenticationFilter;
+
 	@Bean
 	public SecurityFilterChain httpSecurity(HttpSecurity http) throws Exception {
 		return http
-			.csrf().disable()//csrf 비활성화
+			.csrf().disable()
 			.cors()
 			.and()
-			.authorizeRequests()
-			.antMatchers("/**").permitAll()
+			.authorizeHttpRequests()
+			.antMatchers("/tokens").permitAll()
+			.anyRequest().authenticated()
 			.and()
+			.httpBasic().disable()
+			.rememberMe().disable()
+			.formLogin().disable()
+			.logout().disable()
+			.requestCache().disable()
+			.headers().disable()
+			.sessionManagement()
+			.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+			.and()
+			.exceptionHandling()
+			.authenticationEntryPoint(jwtAuthenticationEntryPoint)
+			.and()
+			.addFilterBefore(jwtAuthenticationFilter, OAuth2AuthorizationRequestRedirectFilter.class)
 			.build();
 	}
 
