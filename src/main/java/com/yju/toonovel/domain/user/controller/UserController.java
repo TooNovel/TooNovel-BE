4fdbe7a90fe7aae9d27b0d56aa8ed1bff9090350
태원 <103083087+yestw@users.noreply.github.com>
2023-04-24 10:51:08 +0900

    :sparkles: User 도메인 기능 추가  - register : 추가정보 받은후 회원가입 기능

--- src/main/java/com/yju/toonovel/domain/user/controller/UserController.java
+++ src/main/java/com/yju/toonovel/domain/user/controller/UserController.java
@@ -2,11 +2,14 @@ package com.yju.toonovel.domain.user.controller;
 
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.yju.toonovel.domain.user.dto.UserProfileResponseDto;
+import com.yju.toonovel.domain.user.dto.UserRegisterRequestDto;
 import com.yju.toonovel.domain.user.service.UserService;
 import com.yju.toonovel.global.security.jwt.JwtAuthentication;
 
@@ -28,4 +31,9 @@ public class UserController {
 		return userService.getUserProfile(user.userId);
 	}
 
+	@PatchMapping("/register")
+	public void userRegister(@RequestBody UserRegisterRequestDto requestDto,
+			@AuthenticationPrincipal JwtAuthentication user) {
+		userService.register(user.userId, requestDto);
+	}
 }
