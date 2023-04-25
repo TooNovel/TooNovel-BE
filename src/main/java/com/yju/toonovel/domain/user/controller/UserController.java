438629eabfc523333e7e9ab124d3b3e268c16c84
Hannah0su <luvmyselfh@gmail.com>
2023-04-25 10:49:14 +0900

    TooNovel-BE-17 [FEAT] User 탈퇴 구현 - 탈퇴 시 RefreshToken 삭제 처리

--- src/main/java/com/yju/toonovel/domain/user/controller/UserController.java
+++ src/main/java/com/yju/toonovel/domain/user/controller/UserController.java
@@ -1,6 +1,7 @@
 package com.yju.toonovel.domain.user.controller;
 
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -31,9 +32,14 @@ public class UserController {
 		return userService.getUserProfile(user.userId);
 	}
 
+	@DeleteMapping("/me")
+	public void deleteUser(@AuthenticationPrincipal JwtAuthentication user) {
+		userService.deleteUser(user.userId);
+	}
+
 	@PatchMapping("/register")
 	public void userRegister(@RequestBody UserRegisterRequestDto requestDto,
-			@AuthenticationPrincipal JwtAuthentication user) {
+		@AuthenticationPrincipal JwtAuthentication user) {
 		userService.register(user.userId, requestDto);
 	}
 }
