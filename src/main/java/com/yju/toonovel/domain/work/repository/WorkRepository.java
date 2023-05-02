5ee2c6f031053a89b830b679739d38ec499faf97
Hannah0su <luvmyselfh@gmail.com>
2023-05-02 11:50:38 +0900

    :fire: 기존 Work 관련 코드 제거

--- src/main/java/com/yju/toonovel/domain/work/repository/WorkRepository.java
+++
@@ -1,8 +0,0 @@
-package com.yju.toonovel.domain.work.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.yju.toonovel.domain.work.entity.Work;
-
-public interface WorkRepository extends JpaRepository<Work, Long> {
-}
