480532abd6978c8f0a6177cb7adcecee2993191a
태원 <103083087+yestw@users.noreply.github.com>
2023-05-04 00:43:40 +0900

    :recycle: CustomException 일부 수정  - getter 삭제

--- src/main/java/com/yju/toonovel/domain/review/exception/DuplicateReviewException.java
+++ src/main/java/com/yju/toonovel/domain/review/exception/DuplicateReviewException.java
@@ -3,9 +3,6 @@ package com.yju.toonovel.domain.review.exception;
 import com.yju.toonovel.global.error.exception.BusinessException;
 import com.yju.toonovel.global.error.exception.ErrorCode;
 
-import lombok.Getter;
-
-@Getter
 public class DuplicateReviewException extends BusinessException {
 	public DuplicateReviewException() {
 		super(ErrorCode.REVIEW_ID_DUPLICATE);
--- src/main/java/com/yju/toonovel/domain/review/exception/ReviewNotFoundException.java
+++ src/main/java/com/yju/toonovel/domain/review/exception/ReviewNotFoundException.java
@@ -3,9 +3,6 @@ package com.yju.toonovel.domain.review.exception;
 import com.yju.toonovel.global.error.exception.BusinessException;
 import com.yju.toonovel.global.error.exception.ErrorCode;
 
-import lombok.Getter;
-
-@Getter
 public class ReviewNotFoundException extends BusinessException {
 	public ReviewNotFoundException() {
 		super(ErrorCode.REVIEW_NOT_FOUND);
