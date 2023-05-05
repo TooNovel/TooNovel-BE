711595e20cf4f96e129d8b9ef75f7df502e0938e
Hannah0su <luvmyselfh@gmail.com>
2023-05-05 12:59:17 +0900

    :wrench: 서버 주소 변수로 관리 - 이전 코드 리뷰 내용 반영 - Cors 설정에 머신러닝 서버 추가

--- src/main/java/com/yju/toonovel/global/config/WebConfig.java
+++ src/main/java/com/yju/toonovel/global/config/WebConfig.java
@@ -1,5 +1,6 @@
 package com.yju.toonovel.global.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
@@ -8,11 +9,19 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @EnableWebMvc
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+	@Value("${server.frontEnd}")
+	private String frontEndServer;
+
+	@Value("${server.backEnd}")
+	private String backEndServer;
+
+	@Value("${server.machineLearning}")
+	private String machineLearningServer;
 
 	@Override
 	public void addCorsMappings(CorsRegistry registry) {
 		registry.addMapping("/**")
-			.allowedOriginPatterns("http://localhost:8080", "http://localhost:3000", "/**")
+			.allowedOriginPatterns(frontEndServer, backEndServer, machineLearningServer, "/**")
 			.allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE")
 			.allowedHeaders("*")
 			.allowCredentials(true);
