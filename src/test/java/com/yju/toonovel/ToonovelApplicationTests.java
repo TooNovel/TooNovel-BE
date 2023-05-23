842cd328fa1d3a53c835506212fab478c57f1e25
Hannah0su <luvmyselfh@gmail.com>
2023-05-23 17:59:44 +0900

    :adhesive_bandage: SpringbootTest 주석처리 - 빌드 오류의 원인으로 추정 - 현재 테스트 코드가 없기 때문에, 우선 주석처리

--- src/test/java/com/yju/toonovel/ToonovelApplicationTests.java
+++ src/test/java/com/yju/toonovel/ToonovelApplicationTests.java
@@ -1,9 +1,8 @@
 package com.yju.toonovel;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class ToonovelApplicationTests {
 
 	@Test
