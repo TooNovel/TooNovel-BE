3545e28f18ca9975b65031732bd706e0280eeff0
YJU-KimJeongSu <103088862+YJU-KimJeongSu@users.noreply.github.com>
2023-05-25 13:16:01 +0900

    :poop: 작가 -> 독자 채팅 데모 구현 - 작가 -> 독자 채팅, 독자 -> 작가 채팅 2개로 분리 - Handshake 시 토큰 유효성 인증 - Interceptor가 추가될 가능성을 염두해 폴더 이동 - Dto는 임시로 구성하였으나, 이후 변경 필요 - DB 관련 구현은 추후 구현 예정

--- src/main/java/com/yju/toonovel/global/websocket/controller/ChatController.java
+++ src/main/java/com/yju/toonovel/global/websocket/controller/ChatController.java
@@ -1,11 +1,16 @@
 package com.yju.toonovel.global.websocket.controller;
 
+import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.yju.toonovel.global.websocket.service.ChatService;
+import com.yju.toonovel.global.security.jwt.JwtAuthentication;
+import com.yju.toonovel.global.security.jwt.JwtAuthenticationToken;
+import com.yju.toonovel.global.security.jwt.service.TokenService;
+import com.yju.toonovel.global.websocket.dto.ChatByUserToAuthorDto;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -17,11 +22,21 @@ import lombok.extern.slf4j.Slf4j;
 public class ChatController {
 
 	private final SimpMessagingTemplate messagingTemplate; // @EnableWebSocketMessageBroker 에 의해 등록된 Bean
-	private final ChatService chatService;
+	private final TokenService tokenService;
 
-	@MessageMapping("/chat")
-	public void chat(String temp) {
-		log.info(temp);
+	// 채팅방에 보내기 (작가 -> 독자들). 작동 확인 완료
+	@MessageMapping("/chat/{roomId}")
+	public void chatToReader(@Payload ChatByUserToAuthorDto dto, @DestinationVariable String roomId) {
+		JwtAuthenticationToken token = tokenService.getAuthentication(dto.getSenderAccessToken());
+		JwtAuthentication user = (JwtAuthentication) token.getPrincipal();
+		dto.setSenderId(user.userId);
+		log.info("--------------ChatController.chatToReader : " + dto + "Room ID: " + roomId);
+		messagingTemplate.convertAndSend("/chat/" + roomId, dto.getMessage());
 	}
 
+	// 개인에게 보내기 (독자 -> 작가)
+	@MessageMapping("/chat")
+	public void chatToAuthor(@Payload ChatByUserToAuthorDto dto) {
+		log.info("-----------ChatController.chatToAuthor : " + dto.toString());
+	}
 }
--- src/main/java/com/yju/toonovel/global/websocket/interceptor/StompHandler.java
+++ src/main/java/com/yju/toonovel/global/websocket/interceptor/StompHandler.java
@@ -1,4 +1,4 @@
-package com.yju.toonovel.global.websocket;
+package com.yju.toonovel.global.websocket.interceptor;
 
 import org.springframework.messaging.Message;
 import org.springframework.messaging.MessageChannel;
@@ -7,7 +7,10 @@ import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
 import org.springframework.messaging.support.ChannelInterceptor;
 import org.springframework.stereotype.Component;
 
+import com.yju.toonovel.global.security.jwt.JwtAuthentication;
+import com.yju.toonovel.global.security.jwt.JwtAuthenticationToken;
 import com.yju.toonovel.global.security.jwt.JwtTokenProvider;
+import com.yju.toonovel.global.security.jwt.service.TokenService;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -17,17 +20,26 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class StompHandler implements ChannelInterceptor {
 	private final JwtTokenProvider jwtTokenProvider;
+	private final TokenService tokenService;
 
 	@Override
 	public Message<?> preSend(Message<?> message, MessageChannel channel) {
 		StompHeaderAccessor accessor = StompHeaderAccessor.wrap(message);
+		JwtAuthentication user = null;
 
 		if (accessor.getCommand() == StompCommand.CONNECT) {
 			// 유효한 토큰인지 확인. http 프로토콜이 아니므로 따로 적용해야 함
 			jwtTokenProvider.validateToken(accessor.getFirstNativeHeader("Authorization"));
+			JwtAuthenticationToken token =
+				tokenService.getAuthentication(accessor.getFirstNativeHeader("Authorization"));
+			user = (JwtAuthentication) token.getPrincipal();
 		}
 
-		log.info("-------------StompHandler.preSend---------------");
+		try {
+			log.info("-------------StompHandler.preSend userId:" + user.userId + "---------------");
+		} catch (NullPointerException e) {
+			log.info("-------------StompHandler.preSend---------------");
+		}
 		return message;
 	}
 }
