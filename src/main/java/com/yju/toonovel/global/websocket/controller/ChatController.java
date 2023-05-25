e7484962cecebfec5b84506c3a3f8b2d9bf917e4
YJU-KimJeongSu <103088862+YJU-KimJeongSu@users.noreply.github.com>
2023-05-25 20:29:55 +0900

    :art: 채팅 구조 변경 - 독자 -> 작가 / 작가 -> 독자 통합 (프론트엔드에서 처리) - security에 막히던 버그 수정 - Dto 내용 수정

--- src/main/java/com/yju/toonovel/global/websocket/controller/ChatController.java
+++ src/main/java/com/yju/toonovel/global/websocket/controller/ChatController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.yju.toonovel.global.security.jwt.JwtAuthentication;
 import com.yju.toonovel.global.security.jwt.JwtAuthenticationToken;
 import com.yju.toonovel.global.security.jwt.service.TokenService;
-import com.yju.toonovel.global.websocket.dto.ChatByUserToAuthorDto;
+import com.yju.toonovel.global.websocket.dto.ChatDto;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -21,22 +21,15 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class ChatController {
 
-	private final SimpMessagingTemplate messagingTemplate; // @EnableWebSocketMessageBroker 에 의해 등록된 Bean
+	private final SimpMessagingTemplate messagingTemplate;
 	private final TokenService tokenService;
 
-	// 채팅방에 보내기 (작가 -> 독자들). 작동 확인 완료
 	@MessageMapping("/chat/{roomId}")
-	public void chatToReader(@Payload ChatByUserToAuthorDto dto, @DestinationVariable String roomId) {
+	public void chatToReader(@Payload ChatDto dto, @DestinationVariable String roomId) {
 		JwtAuthenticationToken token = tokenService.getAuthentication(dto.getSenderAccessToken());
 		JwtAuthentication user = (JwtAuthentication) token.getPrincipal();
-		dto.setSenderId(user.userId);
-		log.info("--------------ChatController.chatToReader : " + dto + "Room ID: " + roomId);
-		messagingTemplate.convertAndSend("/chat/" + roomId, dto.getMessage());
-	}
-
-	// 개인에게 보내기 (독자 -> 작가)
-	@MessageMapping("/chat")
-	public void chatToAuthor(@Payload ChatByUserToAuthorDto dto) {
-		log.info("-----------ChatController.chatToAuthor : " + dto.toString());
+		log.info(user.userId.toString()); // DB Insert용
+		dto.setSenderRole(user.role);
+		messagingTemplate.convertAndSend("/chat/" + roomId, dto);
 	}
 }
