5f409093cf1338fc1707f7bf0ecdc21c182343c4
태원 <103083087+yestw@users.noreply.github.com>
2023-06-01 19:55:15 +0900

    :fire: 불필요한 코드 삭제

--- src/main/java/com/yju/toonovel/domain/admin/repository/AdminRepository.java
+++
@@ -1,8 +0,0 @@
-package com.yju.toonovel.domain.admin.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.yju.toonovel.domain.admin.entity.EnrollHistory;
-
-public interface AdminRepository extends JpaRepository<EnrollHistory, Long> {
-}
