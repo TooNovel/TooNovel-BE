73d0105032fa29037dd661c988496fd14a04e3d6
태원 <103083087+yestw@users.noreply.github.com>
2023-06-21 16:57:27 +0900

    Revert ":bug: 내 작품 통계 조회시 버그 발생"
    This reverts commit 1784c3b1410df99b1f4cae01ba474bf468e9f468.

--- src/main/java/com/yju/toonovel/domain/statistics/repository/StatisticsRepository.java
+++ src/main/java/com/yju/toonovel/domain/statistics/repository/StatisticsRepository.java
@@ -10,6 +10,7 @@ import com.yju.toonovel.domain.novel.entity.Novel;
 
 public interface StatisticsRepository extends JpaRepository<Novel, Long> {
 
+	//작가가 쓴 작품인지 검증 - 추후 작가인지 권한도 검증할 예정
 	@Query("select n from Novel n where n.novelId = :nid and n.user.userId = :uid")
 	Optional<Novel> findNovelByUserIdAndNovelId(@Param("nid") Long nid, @Param("uid") Long uid);
 }
