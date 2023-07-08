f1d8e47fc42d6a2a1b9fac0cf26602fdfc6911e5
태원 <103083087+yestw@users.noreply.github.com>
2023-07-09 00:07:31 +0900

    :adhesive_bandage: Dto validation 어노테이션 수정

--- src/main/java/com/yju/toonovel/domain/review/dto/ReviewRegisterRequestDto.java
+++ src/main/java/com/yju/toonovel/domain/review/dto/ReviewRegisterRequestDto.java
@@ -4,6 +4,7 @@ package com.yju.toonovel.domain.review.dto;
 import javax.validation.constraints.Max;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 
 import org.hibernate.validator.constraints.Length;
 
@@ -24,7 +25,7 @@ public class ReviewRegisterRequestDto {
 	@Length(max = 1000)
 	private String reviewContent;
 	@Schema(description = "평점")
-	@NotBlank
+	@NotNull
 	@Max(value = 5)
 	@Min(value = 0)
 	private int reviewGrade;
